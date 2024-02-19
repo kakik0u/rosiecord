e01cdc9d0c2080ee52373ee6fe160c0708319183
SerStars <90714930+SerStars@users.noreply.github.com>
2024-02-19 15:17:57 +0100

    Update constants.js
    Credits to Mugman for the script/automation

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_218.0_55395.ipa";
+Constants.IPA_FETCH_LINK = process.argv[process.argv.length-1];
 export { Shell, Colors, Divider, States, Constants };
